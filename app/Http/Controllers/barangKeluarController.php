a8bec8c3903d0c08880d34494d4c996e55f70d7c
sadikkebalcc <sadiknation2@gmail.com>
2024-12-17 04:57:04 +0000

    pertemuan 32

--- app/Http/Controllers/barangKeluarController.php
+++ app/Http/Controllers/barangKeluarController.php
@@ -7,6 +7,7 @@ use App\Models\barangKeluarController as ModelsBarangKeluarController;
 use App\Models\pelanggan;
 use App\Models\stok;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class barangKeluarController extends Controller
 {
@@ -33,13 +34,15 @@ class barangKeluarController extends Controller
             $date = now()->format('d/m/Y');
             $kode_transaksi = 'TRK' .$nextId . '/' . $date;
             $pelanggan = pelanggan::all();
+            
+            return view('barangKeluar.addbarangKeluar', compact(
+                'data',
+                'kode_transaksi',
+                'pelanggan'
+            ));
         }
         
-        return view('barangKeluar.addbarangKeluar', compact(
-            'data',
-            'kode_transaksi',
-            'pelanggan'
-        ));
+        
         
         $latestItem = ModelsBarangKeluarController::latest()->first();
         $id = $latestItem->id;
@@ -96,30 +99,69 @@ class barangKeluarController extends Controller
     }
 
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+    public function saveProcess(Request $request){
+        $request->validate([
+            'kode_transaksi'=> 'required',
+            'tgl_faktur'=> 'required',
+            'tgl_jatuh_tempo'=> 'required',
+            'pelanggan_id'=> 'required',
+            'jenis_pembayaran'=> 'required',
+            'barang_id'=> 'required',
+            'jumlah_beli'=> 'required',
+            'harga_jual'=> 'required',
+        ], [
+            'kode_transaksi.required' => 'Data wajib diisi',
+            'tgl_faktur.required' => 'Data wajib diisi',
+            'tgl_jatuh_tempo.required' => 'Data wajib diisi',
+            'pelanggan_id.required' => 'Data wajib diisi',
+            'jenis_pembayaran.required' => 'Data wajib diisi',
+            'barang_id.required' => 'Data wajib diisi',
+            'jumlah_beli.required' => 'Data wajib diisi',
+            'harga_jual.required' => 'Data wajib diisi',
+        ]);
+    
+        $save = new ModelsBarangKeluarController();
+        $save->kode_transaksi = $request->kode_transaksi;
+        $save->tgl_faktur = $request->tgl_faktur;
+        $save->tgl_jatuh_tempo = $request->tgl_jatuh_tempo;
+        $save->pelanggan_id = $request->pelanggan_id;
+        $save->jenis_pembayaran = $request->jenis_pembayaran;
+        $save->barang_id = $request->barang_id;
+        $save->jumlah_beli = $request->jumlah_beli;
+
+            //update jumlah stok yang ada didalam database stok
+            $getStokData = stok::find($request->barang_id);
+                $getSumStokNow = $getStokData->stok;
+            $getStokData->stok = $getSumStokNow - $request->jumlah_beli;
+            $getStokData->save();
+        $save->harga_jual = $request->harga_jual;
+
+        $diskon = $request->diskon;
+        $nilaiDiskon = $diskon/100;
+        if ($diskon) {
+            $save->diskon = $request->diskon;
+                $hitung = $request->jumlah_beli * $request->harga_jual;
+                $totalDiskon = $hitung * $nilaiDiskon;
+                $subTotal = $hitung * $totalDiskon;
+
+            $save->sub_total = $subTotal;
+        }else {
+                $hitung = $request->jumlah_beli * $request->harga_jual;
+                $save->sub_total = $hitung;
+            
+            }
+            
+            $save->user_id = Auth::user()->id;
+            $save->tgl_buat = $request->tgl_faktur;
+            $save->save();
+
+            return redirect('/barang-keluar')->with(
+                'message',
+                'Berhasil input barang keluar'
+            );
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
+    
     /**
      * Remove the specified resource from storage.
      */
--- routes/web.php
+++ routes/web.php
@@ -87,6 +87,8 @@ Route::middleware(['auth', 'cekLevel:superadmin,admin'])->group(function(){
         
         Route::get('/barang-keluar/add', 'create');
         Route::post('/barang-keluar/add', 'store');
+        
+        Route::post('/barang-keluar/save', 'saveProcess')->name('addBarangKeluar');
     
         
     });
